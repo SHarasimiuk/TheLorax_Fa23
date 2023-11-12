b3dfc1e4b0e586f308606b60274cb2cbcc92c3c7
SHarasimiuk <149627468+SHarasimiuk@users.noreply.github.com>
2023-11-12 01:04:52 -0600

    Update TheLorax.ino

--- TheLorax.ino
+++ TheLorax.ino
@@ -14,6 +14,18 @@ const int Switch4 = 7;
 const int Switch5 = 4;
 const int Switch6 = 2;
 
+// Variables for the Switch state of Switches 3, 4, and 5
+int Switch3State = 0;
+int LastSwitch3State = 0;
+int Switch4State = 0;
+int LastSwitch4State = 0;
+int Switch5State = 0;
+int LastSwitch5State = 0;
+
+// Creating variables for counting how many times Switches 3 and 4 are activated
+int Switch3Count = 0;
+int Switch4Count = 0;
+
 void setup() {
   // Assigning each Servo an Arduino pin
   Servo1.attach(9);
@@ -34,15 +46,66 @@ void setup() {
   Servo2.write(0);
   Servo3.write(0);
   Servo4.write(0);
+
+  // Set up serial communication
+  Serial.begin(9600);
 }
 
 void loop() {
   // First interaction (open door)
-  if (digitalRead(Switch1) == HIGH){
+  if (digitalRead(Switch1) == HIGH) {
     Servo1.write(90);
   }
+
   // Second interaction (close door)
-  if (digitalRead(Switch2) == HIGH){
+  if (digitalRead(Switch2) == HIGH) {
     Servo1.write(0);
+  }
+
+  // Third interaction (cutting tree 1)
+  Switch3State = digitalRead(Switch3);
+  if (Switch3State != LastSwitch3State) {
+    if (Switch3State == HIGH) {
+      Switch3Count++;
+    }
+  }
+  LastSwitch3State = Switch3State;
+  if (Switch3Count == 4) {
+    Servo2.write(180);
+  }
+
+  // Fourth interaction (cutting tree 2)
+  Switch4State = digitalRead(Switch4);
+  if (Switch4State != LastSwitch4State) {
+    if (Switch4State == HIGH) {
+      Switch4Count++;
+    }
+  }
+  LastSwitch4State = Switch4State;
+  if (Switch4Count == 4) {
+    Servo3.write(180);
+  }
+
+  // Fifth interaction (backdrop change) + enclosure reset
+  Switch5State = digitalRead(Switch5);
+  if (Switch5State == HIGH) {
+    Servo4.write(90);
+    LastSwitch5State = Switch5State;
+  } else {
+    if (Switch5State != LastSwitch5State) {
+      if (digitalRead(Switch6) == LOW) {
+        delay(2000);
+        Servo4.write(0);
+        Servo3.write(0);
+        Servo2.write(0);
+      }
+    }
+  }
+
+  // Sixth interaction (ignoring the Lorax)
+  if (digitalRead(Switch6) == HIGH) {
+    if (digitalRead(Switch5) == HIGH) {
+      Servo4.write(180);
+    }
   }
 }
